b3d505810b43f3a23d0cb4c46cf061b5a891ed5a
kathirvel13 <kathir13@proton.me>
2025-02-28 23:03:21 +0530

    -m

---
+++ autocommit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Shell: $SHELL"
+echo "User: $USER"
+echo "Directory: $PWD"
+echo "Git User: $(git config user.name)"
+echo "Git mail: $(git config user.email)"
+echo
+
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+	echo "./autocommit.sh <argument 1>"
+	echo "-d	--diff		git diff"
+	echo "-l	--log		git log --oneline"
+	echo "-h	--help		Help"; echo;
+	exit 0
+fi
+
+echo "$(git status)"; echo;
+
+if [[ $1 == "-d" || $1 == "--diff" ]]; then
+	echo "$(git diff)"; echo;
+	exit 0
+elif [[ $1 == "-l" || $1 == "--log" ]]; then
+	echo "$(git log --oneline)"; echo;
+	exit 0
+fi
+
+if [[ $(git status) != "On branch main nothing to commit, working tree clean" ]]; then
+	git add .
+	echo "Enter commit:"; read desc1;
+	echo "Enter Description:"; read desc2;
+	echo "$(git commit -m $disc1 -m $disc2)"; echo;
+	echo "$(git push)"; echo;
+fi
