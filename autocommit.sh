0868d9a868528d09524e3fe79505033cbfecf6c8
kathirvel13 <kathir13@proton.me>
2025-03-01 16:13:38 +0530

    $disc1
    $disc2

--- autocommit.sh
+++ autocommit.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/bash
+
+cd $(dirname $(realpath $0))
 
 echo "Shell: $SHELL"
 echo "User: $USER"
@@ -11,24 +13,26 @@ if [[ $1 == "-h" || $1 == "--help" ]]; then
 	echo "./autocommit.sh <argument 1>"
 	echo "-d	--diff		git diff"
 	echo "-l	--log		git log --oneline"
-	echo "-h	--help		Help"; echo;
+	echo "-h	--help		Help"
 	exit 0
 fi
 
 echo "$(git status)"; echo;
 
 if [[ $1 == "-d" || $1 == "--diff" ]]; then
-	echo "$(git diff)"; echo;
+	echo "$(git diff)"
 	exit 0
 elif [[ $1 == "-l" || $1 == "--log" ]]; then
-	echo "$(git log --oneline)"; echo;
+	echo "$(git log --oneline)"
 	exit 0
 fi
 
-if [[ $(git status) != "On branch main Your branch is up to date with 'origin/main'. nothing to commit, working tree clean" ]]; then
-	git add .
+if [[ 1 ]]; then
 	echo "Enter commit:"; read desc1;
 	echo "Enter Description:"; read desc2;
-	echo "$(git commit -m $disc1 -m $disc2)"; echo;
-	echo "$(git push)"; echo;
+	git add .
+	echo "$(git commit -m '$disc1' -m '$disc2')"; echo;
+	echo "$(git push)"
+else
+	exit 0
 fi
